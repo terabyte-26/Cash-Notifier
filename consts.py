6e6f78c74399d1c6bffbb6f81e3fea2d2ab68135
Wmerl <mahibni@outlook.com>
2025-04-10 23:10:51 +0100

    ADD: consts.py that contains all the Consts of the App

---
+++ consts.py
@@ -0,0 +1,22 @@
+# Written by Hamza Farahat <farahat.hamza1@gmail.com>, 4/10/2025
+# Contact me for more information:
+# Contact Us: https://terabyte-26.com/quick-links/
+# Telegram: @hamza_farahat or https://t.me/hamza_farahat
+# WhatsApp: +212772177012
+
+
+class HtmlElements(object):
+
+    HISTORY_BTN: str = '//*[@id="main-content"]/div[1]/div/div/div/div/div[1]/div[2]/div[1]/div[1]/button'
+
+
+class URLs(object):
+    """
+    URLs used in the project
+    """
+
+    STAKE_BASE_URL: str = "https://stake.com"
+    CRASH_URL: str = f"{STAKE_BASE_URL}/casino/games/crash"
+    STAKE_GRAPHQL_URL: str = f"{STAKE_BASE_URL}/_api/graphql"
+
+    CHROME_DEV_TOOLS_URL: str = "http://localhost:9222"
